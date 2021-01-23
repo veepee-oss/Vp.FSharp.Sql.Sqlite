ed334e7064ee2e761c5b474f78c6e54faf896ec7
Kerry Perret <kerry.perret@outlook.com>
2021-01-23 17:09:58 +0100

    feat(refactor): add specific tc support

---
+++ Vp.FSharp.Sql.Sqlite/SqliteNullDbValue.fs
@@ -0,0 +1,8 @@
+﻿[<RequireQualifiedAccess>]
+module Vp.FSharp.Sql.Sqlite.SqliteNullDbValue
+
+open Vp.FSharp.Sql
+
+
+let ifNone toDbValue = NullDbValue.ifNone toDbValue SqliteDbValue.Null
+let ifError toDbValue = NullDbValue.ifError toDbValue (fun _ -> SqliteDbValue.Null)
---
+++ Vp.FSharp.Sql.Sqlite/SqliteTransaction.fs
@@ -0,0 +1,22 @@
+﻿[<RequireQualifiedAccess>]
+module Vp.FSharp.Sql.SqlServer.SqliteTransaction
+
+open Vp.FSharp.Sql
+open Vp.FSharp.Sql.Sqlite
+
+
+let beginTransactionAsync = Constants.Deps.BeginTransactionAsync
+
+let commit cancellationToken isolationLevel connection body =
+    Transaction.commit cancellationToken isolationLevel connection beginTransactionAsync body
+let notCommit cancellationToken isolationLevel connection body =
+    Transaction.notCommit cancellationToken isolationLevel connection beginTransactionAsync body
+let commitOnOk cancellationToken isolationLevel connection body =
+    Transaction.commitOnOk cancellationToken isolationLevel connection beginTransactionAsync body
+let commitOnSome cancellationToken isolationLevel connection body =
+    Transaction.commitOnSome cancellationToken isolationLevel connection beginTransactionAsync body
+
+let defaultCommit connection body = Transaction.defaultCommit connection beginTransactionAsync body
+let defaultNotCommit connection body = Transaction.defaultNotCommit connection beginTransactionAsync body
+let defaultCommitOnOk connection body = Transaction.defaultCommitOnOk connection beginTransactionAsync body
+let defaultCommitOnSome connection body = Transaction.defaultCommitOnSome connection beginTransactionAsync body
