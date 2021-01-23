a31ad81b32079cc23ee1f081ceed575deafd9d55
Natalie Perret <natalie.perret.1986@gmail.com>
2021-01-23 17:09:58 +0100

    feat(refactor): add specific tc support

--- Vp.FSharp.Sql.Sqlite/SqliteCommand.fs
+++ Vp.FSharp.Sql.Sqlite/SqliteCommand.fs
@@ -1,75 +1,11 @@
 ﻿namespace Vp.FSharp.Sql.Sqlite
 
-open System.Data.SQLite
-open System.Threading.Tasks
-
 open Vp.FSharp.Sql
 
-
-/// Native SQLite DB types.
-/// See https://www.sqlite.org/datatype3.html
-type SqliteDbValue =
-    | Null
-    | Integer of int64
-    | Real of double
-    | Text of string
-    | Blob of byte array
-
-type SqliteCommandDefinition =
-    CommandDefinition<
-        SQLiteConnection,
-        SQLiteCommand,
-        SQLiteParameter,
-        SQLiteDataReader,
-        SQLiteTransaction,
-        SqliteDbValue>
-
-type SqliteConfiguration =
-    SqlConfigurationCache<
-        SQLiteConnection,
-        SQLiteCommand>
-
-type SqliteDependencies =
-    SqlDependencies<
-        SQLiteConnection,
-        SQLiteCommand,
-        SQLiteParameter,
-        SQLiteDataReader,
-        SQLiteTransaction,
-        SqliteDbValue>
-
-[<RequireQualifiedAccess>]
-module SqliteNullDbValue =
-    let ifNone toDbValue = NullDbValue.ifNone toDbValue SqliteDbValue.Null
-    let ifError toDbValue = NullDbValue.ifError toDbValue (fun _ -> SqliteDbValue.Null)
 
 [<RequireQualifiedAccess>]
 module SqliteCommand =
 
-    let private dbValueToParameter name value =
-        let parameter = SQLiteParameter()
-        parameter.ParameterName <- name
-        match value with
-        | Null ->
-            parameter.TypeName <- (nameof Null).ToUpperInvariant()
-        | Integer value ->
-            parameter.TypeName <- (nameof Integer).ToUpperInvariant()
-            parameter.Value <- value
-        | Real value ->
-            parameter.TypeName <- (nameof Real).ToUpperInvariant()
-            parameter.Value <- value
-        | Text value ->
-            parameter.TypeName <- (nameof Text).ToUpperInvariant()
-            parameter.Value <- value
-        | Blob value ->
-            parameter.TypeName <- (nameof Blob).ToUpperInvariant()
-            parameter.Value <- value
-        parameter
-
-    let private deps: SqliteDependencies =
-        { CreateCommand = fun connection -> connection.CreateCommand()
-          ExecuteReaderAsync = fun command _ -> Task.FromResult(command.ExecuteReader())
-          DbValueToParameter = dbValueToParameter }
 
     /// Initialize a command definition with the given text contained in the given string.
     let text value : SqliteCommandDefinition =
---
+++ Vp.FSharp.Sql.Sqlite/Types.fs
@@ -0,0 +1,76 @@
+﻿namespace Vp.FSharp.Sql.Sqlite
+
+open System.Data
+open System.Data.SQLite
+open System.Threading.Tasks
+
+open Vp.FSharp.Sql
+
+
+/// Native SQLite DB types.
+/// See https://www.sqlite.org/datatype3.html
+type SqliteDbValue =
+    | Null
+    | Integer of int64
+    | Real of double
+    | Text of string
+    | Blob of byte array
+
+type SqliteCommandDefinition =
+    CommandDefinition<
+        SQLiteConnection,
+        SQLiteCommand,
+        SQLiteParameter,
+        SQLiteDataReader,
+        SQLiteTransaction,
+        SqliteDbValue>
+
+type SqliteConfiguration =
+    SqlConfigurationCache<
+        SQLiteConnection,
+        SQLiteCommand>
+
+type SqliteDependencies =
+    SqlDependencies<
+        SQLiteConnection,
+        SQLiteCommand,
+        SQLiteParameter,
+        SQLiteDataReader,
+        SQLiteTransaction,
+        SqliteDbValue>
+
+[<AbstractClass; Sealed>]
+type internal Constants private () =
+
+    static member DbValueToParameter name value =
+        let parameter = SQLiteParameter()
+        parameter.ParameterName <- name
+        match value with
+        | Null ->
+            parameter.TypeName <- (nameof Null).ToUpperInvariant()
+        | Integer value ->
+            parameter.TypeName <- (nameof Integer).ToUpperInvariant()
+            parameter.Value <- value
+        | Real value ->
+            parameter.TypeName <- (nameof Real).ToUpperInvariant()
+            parameter.Value <- value
+        | Text value ->
+            parameter.TypeName <- (nameof Text).ToUpperInvariant()
+            parameter.Value <- value
+        | Blob value ->
+            parameter.TypeName <- (nameof Blob).ToUpperInvariant()
+            parameter.Value <- value
+        parameter
+
+    static member Deps : SqliteDependencies =
+        let beginTransactionAsync (connection: SQLiteConnection) (isolationLevel: IsolationLevel) _ =
+            ValueTask.FromResult(connection.BeginTransaction(isolationLevel))
+
+        let executeReaderAsync (command: SQLiteCommand) _ =
+            Task.FromResult(command.ExecuteReader())
+
+        { CreateCommand = fun connection -> connection.CreateCommand()
+          SetCommandTransaction = fun command transaction -> command.Transaction <- transaction
+          BeginTransactionAsync = beginTransactionAsync
+          ExecuteReaderAsync = executeReaderAsync
+          DbValueToParameter = Constants.DbValueToParameter }
