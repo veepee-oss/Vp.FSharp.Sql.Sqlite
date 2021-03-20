b84978eb6d95a28485987578736eb385e4198983
Mary Perret <mary.perret.1986@gmail.com>
2021-03-20 23:57:01 +0100

    feat(author): update

--- Vp.FSharp.Sql.Sqlite/SqliteTransaction.fs
+++ Vp.FSharp.Sql.Sqlite/SqliteTransaction.fs
@@ -7,16 +7,38 @@ open Vp.FSharp.Sql.Sqlite
 
 let private beginTransactionAsync = Constants.Deps.BeginTransactionAsync
 
+/// Create and commit an automatically generated transaction with the given connection, isolation,
+/// cancellation token and transaction body. 
 let commit cancellationToken isolationLevel connection body =
     Transaction.commit cancellationToken isolationLevel connection beginTransactionAsync body
+
+/// Create and do not commit an automatically generated transaction with the given connection, isolation,
+/// cancellation token and transaction body. 
 let notCommit cancellationToken isolationLevel connection body =
     Transaction.notCommit cancellationToken isolationLevel connection beginTransactionAsync body
+
+/// Create and commit an automatically generated transaction with the given connection, isolation,
+/// cancellation token and transaction body. 
+/// The commit phase only occurs if the transaction body returns Ok.
 let commitOnOk cancellationToken isolationLevel connection body =
     Transaction.commitOnOk cancellationToken isolationLevel connection beginTransactionAsync body
+
+/// Create and commit an automatically generated transaction with the given connection, isolation,
+/// cancellation token and transaction body. 
+/// The commit phase only occurs if the transaction body returns Some.
 let commitOnSome cancellationToken isolationLevel connection body =
     Transaction.commitOnSome cancellationToken isolationLevel connection beginTransactionAsync body
 
+/// Create and commit an automatically generated transaction with the given connection and transaction body. 
 let defaultCommit connection body = Transaction.defaultCommit connection beginTransactionAsync body
+
+/// Create and do not commit an automatically generated transaction with the given connection and transaction body.
 let defaultNotCommit connection body = Transaction.defaultNotCommit connection beginTransactionAsync body
+
+/// Create and commit an automatically generated transaction with the given connection and transaction body.
+/// The commit phase only occurs if the transaction body returns Ok.
 let defaultCommitOnOk connection body = Transaction.defaultCommitOnOk connection beginTransactionAsync body
+
+/// Create and commit an automatically generated transaction with the given connection and transaction body.
+/// The commit phase only occurs if the transaction body returns Some.
 let defaultCommitOnSome connection body = Transaction.defaultCommitOnSome connection beginTransactionAsync body
