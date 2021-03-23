f9c0a1d7bc1e40586cde519b9912456d89d4d880
Natalie Perret <natalie.perret.1986@gmail.com>
2021-03-23 10:18:34 +0100

    feat(docs): readme++

--- Vp.FSharp.Sql.Sqlite/SqliteCommand.fs
+++ Vp.FSharp.Sql.Sqlite/SqliteCommand.fs
@@ -42,46 +42,107 @@ let transaction value (commandDefinition: SqliteCommandDefinition) : SqliteComma
     SqlCommand.transaction value commandDefinition
 
 /// Execute the command and return the sets of rows as an AsyncSeq accordingly to the command definition.
+/// This function runs asynchronously.
 let queryAsyncSeq connection read (commandDefinition: SqliteCommandDefinition) =
     SqlCommand.queryAsyncSeq
         connection (Constants.Deps) (SqliteConfiguration.Snapshot) read commandDefinition
 
+/// Execute the command and return the sets of rows as an AsyncSeq accordingly to the command definition.
+/// This function runs synchronously.
+let querySeqSync connection read (commandDefinition: SqliteCommandDefinition) =
+    SqlCommand.querySeqSync
+        connection (Constants.Deps) (SqliteConfiguration.Snapshot) read commandDefinition
+
 /// Execute the command and return the sets of rows as a list accordingly to the command definition.
+/// This function runs asynchronously.
 let queryList connection read (commandDefinition: SqliteCommandDefinition) =
     SqlCommand.queryList
         connection (Constants.Deps) (SqliteConfiguration.Snapshot) read commandDefinition
 
+/// Execute the command and return the sets of rows as a list accordingly to the command definition.
+/// This function runs synchronously.
+let queryListSync connection read (commandDefinition: SqliteCommandDefinition) =
+    SqlCommand.queryListSync
+        connection (Constants.Deps) (SqliteConfiguration.Snapshot) read commandDefinition
+
 /// Execute the command and return the first set of rows as a list accordingly to the command definition.
+/// This function runs asynchronously.
 let querySetList connection read (commandDefinition: SqliteCommandDefinition) =
     SqlCommand.querySetList
         connection (Constants.Deps) (SqliteConfiguration.Snapshot) read commandDefinition
 
+/// Execute the command and return the first set of rows as a list accordingly to the command definition.
+/// This function runs synchronously.
+let querySetListSync connection read (commandDefinition: SqliteCommandDefinition) =
+    SqlCommand.querySetListSync
+        connection (Constants.Deps) (SqliteConfiguration.Snapshot) read commandDefinition
+
 /// Execute the command and return the 2 first sets of rows as a tuple of 2 lists accordingly to the command definition.
+/// This function runs asynchronously.
 let querySetList2 connection read1 read2 (commandDefinition: SqliteCommandDefinition) =
     SqlCommand.querySetList2
         connection (Constants.Deps) (SqliteConfiguration.Snapshot) read1 read2 commandDefinition
 
+/// Execute the command and return the 2 first sets of rows as a tuple of 2 lists accordingly to the command definition.
+/// This function runs synchronously.
+let querySetList2Sync connection read1 read2 (commandDefinition: SqliteCommandDefinition) =
+    SqlCommand.querySetList2Sync
+        connection (Constants.Deps) (SqliteConfiguration.Snapshot) read1 read2 commandDefinition
+
 /// Execute the command and return the 3 first sets of rows as a tuple of 3 lists accordingly to the command definition.
+/// This function runs asynchronously.
 let querySetList3 connection read1 read2 read3 (commandDefinition: SqliteCommandDefinition) =
     SqlCommand.querySetList3
+        connection (Constants.Deps) (SqliteConfiguration.Snapshot) read1 read2 read3 commandDefinition
+
+/// Execute the command and return the 3 first sets of rows as a tuple of 3 lists accordingly to the command definition.
+/// This function runs synchronously.
+let querySetList3Sync  connection read1 read2 read3 (commandDefinition: SqliteCommandDefinition) =
+    SqlCommand.querySetList3Sync
         connection (Constants.Deps) (SqliteConfiguration.Snapshot) read1 read2 read3 commandDefinition
 
 /// Execute the command accordingly to its definition and,
 /// - return the first cell value, if it is available and of the given type.
 /// - throw an exception, otherwise.
+/// This function runs asynchronously.
 let executeScalar<'Scalar> connection (commandDefinition: SqliteCommandDefinition) =
     SqlCommand.executeScalar<'Scalar, _, _, _, _, _, _, _, _>
+        connection (Constants.Deps) (SqliteConfiguration.Snapshot) commandDefinition
+
+/// Execute the command accordingly to its definition and,
+/// - return the first cell value, if it is available and of the given type.
+/// - throw an exception, otherwise.
+/// This function runs synchronously.
+let executeScalarSync<'Scalar> connection (commandDefinition: SqliteCommandDefinition) =
+    SqlCommand.executeScalarSync<'Scalar, _, _, _, _, _, _, _, _>
         connection (Constants.Deps) (SqliteConfiguration.Snapshot) commandDefinition
 
 /// Execute the command accordingly to its definition and,
 /// - return Some, if the first cell is available and of the given type.
 /// - return None, if first cell is DBNull.
 /// - throw an exception, otherwise.
+/// This function runs asynchronously.
 let executeScalarOrNone<'Scalar> connection (commandDefinition: SqliteCommandDefinition) =
     SqlCommand.executeScalarOrNone<'Scalar, _, _, _, _, _, _, _, _>
         connection (Constants.Deps) (SqliteConfiguration.Snapshot) commandDefinition
 
+/// Execute the command accordingly to its definition and,
+/// - return Some, if the first cell is available and of the given type.
+/// - return None, if first cell is DBNull.
+/// - throw an exception, otherwise.
+/// This function runs synchronously.
+let executeScalarOrNoneSync<'Scalar> connection (commandDefinition: SqliteCommandDefinition) =
+    SqlCommand.executeScalarOrNoneSync<'Scalar, _, _, _, _, _, _, _, _>
+        connection (Constants.Deps) (SqliteConfiguration.Snapshot) commandDefinition
+
 /// Execute the command accordingly to its definition and, return the number of rows affected.
+/// This function runs asynchronously.
 let executeNonQuery connection (commandDefinition: SqliteCommandDefinition) =
     SqlCommand.executeNonQuery
+        connection (Constants.Deps) (SqliteConfiguration.Snapshot) commandDefinition
+
+/// Execute the command accordingly to its definition and, return the number of rows affected.
+/// This function runs synchronously.
+let executeNonQuerySync connection (commandDefinition: SqliteCommandDefinition) =
+    SqlCommand.executeNonQuerySync
         connection (Constants.Deps) (SqliteConfiguration.Snapshot) commandDefinition
--- Vp.FSharp.Sql.Sqlite/SqliteTransaction.fs
+++ Vp.FSharp.Sql.Sqlite/SqliteTransaction.fs
@@ -6,39 +6,92 @@ open Vp.FSharp.Sql.Sqlite
 
 
 let private beginTransactionAsync = Constants.Deps.BeginTransactionAsync
+let private beginTransaction = Constants.Deps.BeginTransaction
 
 /// Create and commit an automatically generated transaction with the given connection, isolation,
 /// cancellation token and transaction body.
+/// This function runs asynchronously.
 let commit cancellationToken isolationLevel connection body =
     Transaction.commit cancellationToken isolationLevel connection beginTransactionAsync body
 
+/// Create and commit an automatically generated transaction with the given connection, isolation,
+/// cancellation token and transaction body.
+/// This function runs synchronously.
+let commitSync isolationLevel connection body =
+    Transaction.commitSync isolationLevel connection beginTransaction body
+
 /// Create and do not commit an automatically generated transaction with the given connection, isolation,
 /// cancellation token and transaction body.
+/// This function runs asynchronously.
 let notCommit cancellationToken isolationLevel connection body =
     Transaction.notCommit cancellationToken isolationLevel connection beginTransactionAsync body
+
+/// Create and do not commit an automatically generated transaction with the given connection, isolation,
+/// and transaction body.
+/// This function runs synchronously.
+let notCommitSync isolationLevel connection body =
+    Transaction.notCommitSync isolationLevel connection beginTransaction body
 
 /// Create and commit an automatically generated transaction with the given connection, isolation,
 /// cancellation token and transaction body.
 /// The commit phase only occurs if the transaction body returns Some.
+/// This function runs asynchronously.
 let commitOnSome cancellationToken isolationLevel connection body =
     Transaction.commitOnSome cancellationToken isolationLevel connection beginTransactionAsync body
 
 /// Create and commit an automatically generated transaction with the given connection, isolation,
+/// and transaction body.
+/// The commit phase only occurs if the transaction body returns Some.
+/// This function runs synchronously.
+let commitOnSomeSync isolationLevel connection body =
+    Transaction.commitOnSomeSync isolationLevel connection beginTransaction body
+
+/// Create and commit an automatically generated transaction with the given connection, isolation,
 /// cancellation token and transaction body.
 /// The commit phase only occurs if the transaction body returns Ok.
+/// This function runs asynchronously.
 let commitOnOk cancellationToken isolationLevel connection body =
     Transaction.commitOnOk cancellationToken isolationLevel connection beginTransactionAsync body
 
+/// Create and commit an automatically generated transaction with the given connection, isolation,
+/// and transaction body.
+/// The commit phase only occurs if the transaction body returns Ok.
+/// This function runs synchronously.
+let commitOnOkSync isolationLevel connection body =
+    Transaction.commitOnOkSync isolationLevel connection beginTransaction body
+
 /// Create and commit an automatically generated transaction with the given connection and transaction body.
+/// This function runs asynchronously.
 let defaultCommit connection body = Transaction.defaultCommit connection beginTransactionAsync body
 
+/// Create and commit an automatically generated transaction with the given connection and transaction body.
+/// This function runs synchronously.
+let defaultCommitSync connection body = Transaction.defaultNotCommitSync connection beginTransaction body
+
 /// Create and do not commit an automatically generated transaction with the given connection and transaction body.
+/// This function runs asynchronously.
 let defaultNotCommit connection body = Transaction.defaultNotCommit connection beginTransactionAsync body
+
+/// Create and do not commit an automatically generated transaction with the given connection and transaction body.
+/// This function runs asynchronously.
+let defaultNotCommitSync connection body = Transaction.defaultNotCommitSync connection beginTransaction body
 
 /// Create and commit an automatically generated transaction with the given connection and transaction body.
 /// The commit phase only occurs if the transaction body returns Ok.
+/// This function runs asynchronously.
 let defaultCommitOnSome connection body = Transaction.defaultCommitOnSome connection beginTransactionAsync body
 
 /// Create and commit an automatically generated transaction with the given connection and transaction body.
+/// The commit phase only occurs if the transaction body returns Ok.
+/// This function runs synchronously.
+let defaultCommitOnSomeSync connection body = Transaction.defaultCommitOnSomeSync connection beginTransaction body
+
+/// Create and commit an automatically generated transaction with the given connection and transaction body.
 /// The commit phase only occurs if the transaction body returns Some.
+/// This function runs asynchronously.
 let defaultCommitOnOk connection body = Transaction.defaultCommitOnOk connection beginTransactionAsync body
+
+/// Create and commit an automatically generated transaction with the given connection and transaction body.
+/// The commit phase only occurs if the transaction body returns Some.
+/// This function runs synchronously.
+let defaultCommitOnOkSync connection body = Transaction.defaultCommitOnOkSync connection beginTransaction body
